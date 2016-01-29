b243e13dcb4d2dab3fa2bccbf970a960527f8367
mrandall <mrandall@mark-work.local>
2016-01-29 12:23:06 -0800

    FormInputViewModel: fixed but. updated vm to validate after value is set to nil.

--- Pod/Classes/InputViewModels/FormInputViewModel.swift
+++ Pod/Classes/InputViewModels/FormInputViewModel.swift
@@ -91,6 +91,7 @@ public class FormInputViewModel<T>: FormInputViewModelObservable {
                 }
                 
             } else {
+                valid = validateUsingDisplayValidationErrorsOnValueChangeValue()
                 displayValue = ""
             }
         }
